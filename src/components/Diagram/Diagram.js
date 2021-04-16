090da26142dcf8ad07e040e7272aa2ace4830097
tanvirislamcu <tanvirislam@cunet.carleton.ca>
2021-04-16 13:42:26 -0400

    fix lint

--- src/components/Diagram/Diagram.js
+++ src/components/Diagram/Diagram.js
@@ -1,33 +1,29 @@
 import React from 'react';
 import { Box, makeStyles } from '@material-ui/core';
-import { ReactComponent as SVGDiagram } from '../../assets/images/diagram.svg'
-import * as d3 from 'd3'
+import * as d3 from 'd3';
+import { ReactComponent as SVGDiagram } from '../../assets/images/diagram.svg';
 
-export const useStyles = makeStyles(() => ({
-    root: {
-        padding: 30
-    }
-}));
+export const useStyles = makeStyles(() => ({ root: { padding: 30 } }));
 
 const Diagram = () => {
-    const classes = useStyles()
+    const classes = useStyles();
 
     React.useEffect(() => {
         d3.select('g')
             .selectAll('*')
             .on('click', function() {
-                console.log(this)
+                console.log(this);
                 d3.select(this)
-                    .attr('stroke', 'tomato')
+                    .attr('stroke', 'tomato');
             })
             .attr('pointer-events', 'fill')
-            .style('cursor', 'pointer')
-    }, [])
+            .style('cursor', 'pointer');
+    }, []);
 
     return (
         <Box
-            className={classes.root}
             alignItems='center'
+            className={classes.root}
             display='flex'
             flexDirection='row'
         >

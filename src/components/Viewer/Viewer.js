b3bcc56cec199c3d277435c483a70c97890109e0
Jazmin Romero <jazminromero@Jazmins-MacBook-Pro.local>
2021-04-20 13:03:38 -0400

    [RC-338] Layout Boxes

---
+++ src/components/Viewer/Viewer.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Box, makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles(() => ({
+    root: {
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        border: '1px solid black',
+        padding: 10,
+    },
+    jsonViewer: {
+        position: 'relative',
+        border: '1px solid black',
+        height: '100%',
+        width: '100%',
+        margin: 10,
+        minWidth: 450,
+    },
+    svgViewer: {
+        position: 'relative',
+        border: '1px solid black',
+        height: '100%',
+        width: '100%',
+        margin: 10,
+        minWidth: 450,
+    },
+}));
+
+const Viewer = () => {
+    const classes = useStyles();
+
+    return (
+        <Box
+            className={classes.root}
+            flexDirection='row'
+        >
+            <Box
+                className={classes.jsonViewer}
+            />
+            <Box
+                className={classes.svgViewer}
+            />
+        </Box>
+    );
+};
+
+Viewer.propTypes = {};
+
+Viewer.defaultProps = {};
+
+export default Viewer;
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,10 @@
 import NotFound from '../components/NotFound';
-import Diagram from '../components/Diagram';
+import Viewer from '../components/Viewer';
 
 export default [
     {
         path: '/',
-        component: Diagram,
+        component: Viewer,
     },
     {
         path: '*',
